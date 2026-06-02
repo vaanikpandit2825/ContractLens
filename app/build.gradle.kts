f7d785f3224cd28be389e6f50ed1556461a64fcf
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-06-02 15:25:04 +0530

    Implement clause card adapter and JSON parsin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.tom-roush:pdfbox-android:2.0.27.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
